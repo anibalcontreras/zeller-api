c1ec61f0604cbaf3d94b745d710be4065dcab046
ANIBAL CONTRERAS <anibal.contreras@uc.cl>
2024-11-06 16:20:48 -0300

    Feat - Retrieve clientes to do follow up

--- clients/views.py
+++ clients/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Client
 from .serializers import ClientSerializer, ClientListSerializer
+from rest_framework.decorators import action
+from datetime import timedelta
+from django.utils import timezone
 
 
 class ClientViewSet(viewsets.ModelViewSet):
@@ -17,3 +20,10 @@ class ClientViewSet(viewsets.ModelViewSet):
             {"detail": "You are not allowed to delete clients."},
             status=status.HTTP_405_METHOD_NOT_ALLOWED,
         )
+    
+    @action(detail=False, methods=['get'], url_path='to-do-follow-up')
+    def clients_to_do_follow_up(self, request):
+        seven_days_ago = timezone.now() - timedelta(days=7)
+        clients = Client.objects.filter(conversations__sent_at__lt=seven_days_ago).distinct()
+        serializer = ClientListSerializer(clients, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
